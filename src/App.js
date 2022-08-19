f5034a1bb178dab3522432a3b0b30746119e8132
uheaji <yoolbk@naver.com>
2022-08-19 11:18:36 +0900

    🎨 Styled Components : 'As' and Attrs
    -컴포넌트의 html 태그 변경
    - styled components 에서 속성도 설정가능 !

--- src/App.js
+++ src/App.js
@@ -4,22 +4,18 @@ const Container = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-// Box 컴포넌트의 모든 속성들을 가져 온 다음 추가적으로 아래코드를 더해준다. (깔끔😊)
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Input = styled.input.attrs({required: true, minLength: 10})`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Container>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+    <Container as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Container>
   );
 }
