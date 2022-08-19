0c8eb07b4c1678f8c382ac6c966ad0668272b3cc
uheaji <yoolbk@naver.com>
2022-08-19 10:30:10 +0900

    🎨 Styled Components : Adapting and Extending
    - styled components props 적용
    - styled() 생성자를 이용해 컴포넌트 확장

--- src/App.js
+++ src/App.js
@@ -4,29 +4,22 @@ const Container = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.h1`
-  color: white;
+// Box 컴포넌트의 모든 속성들을 가져 온 다음 추가적으로 아래코드를 더해준다. (깔끔😊)
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Container>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
     </Container>
   );
 }
