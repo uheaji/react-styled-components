e080ffa5752b4b2e9febb1bd3350aba8b701d166
uheaji <yoolbk@naver.com>
2022-08-23 12:04:49 +0900

    🎨 Styled Components : Animations and Pseudo Selectors
    - 애니메이션 효과 (keyframes)
    - 컴포넌트 안 엘리먼트한테도 styled-components 적용

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
 const Container = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({required: true, minLength: 10})`
+// keyframes는 애니메이션 속성 중 하나, 중간에 스타일이 바뀌는 지점 
+const rotationAnimation = keyframes`
+  // 단계별로 변화를 주는 코드
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite; // 1초동안 선형 무한 속성값주기
+  span {
+    font-size: 26px;
+    &:hover {
+      font-size: 36px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Container as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
+    <Container>
+      <Box>
+        <span>🤩</span>
+      </Box>
     </Container>
   );
 }
